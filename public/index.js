9f315372cc28d47c6192e346ada30387ca80373d
Shamima Rahman <Samu@Shamimas-MacBook-Air.local>
2020-10-28 21:49:35 -0400

    first commit

---
+++ public/index.js
@@ -0,0 +1,26 @@
+const dropdown = document.getElementById("myDropdown");
+function myFunction() {
+    dropdown.classList.toggle("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn, .hamburger')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+
