90f4cee824d81db04724d17111f766fe5c332af9
Shamima Rahman <Samu@Shamimas-MacBook-Air.local>
2020-11-17 19:00:07 -0500

    Separated models, views and routes.

---
+++ models/user.js
@@ -0,0 +1,21 @@
+
+var mongoose = require("mongoose");
+require('dotenv').config();
+
+// mongodb connection and code
+mongoose.connect(process.env.DB_CONN);
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+  "email": {
+    type: String,
+    unique: true
+  },
+  "firstName": String,
+  "lastName": String,
+  "password": String,
+  "birthdate": Date  
+});
+
+var User = mongoose.model("users", userSchema)
+module.exports = User;
